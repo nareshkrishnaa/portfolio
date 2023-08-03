e40b44c6221a163abf4854eab3ae12f90a133604
nareshkrishnaa <96198528+nareshkrishnaa@users.noreply.github.com>
2023-08-04 00:25:09 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/game.js
@@ -0,0 +1,201 @@
+  // Get references to the button and audio element
+ var w1 = document.getElementById("w1");
+ var w = document.getElementById("w");
+
+ // Add a click event listener to the button
+ w1.addEventListener("click", function() {
+     w.play();
+     var button = document.querySelector("#w1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+
+ });
+
+ document.addEventListener("keydown", function(event) {
+    var key = event.key.toLowerCase();
+    if (key === "w") {
+        // Play the audio
+        w.play();
+
+     var button = document.querySelector("#w1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+    }
+});
+
+
+  // Get references to the button and audio element
+  var a1 = document.getElementById("a1");
+  var a = document.getElementById("a");
+ 
+  // Add a click event listener to the button
+  a1.addEventListener("click", function() {
+      a.play();
+
+      var button = document.querySelector("#a1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+  });
+  document.addEventListener("keydown", function(event) {
+    var key = event.key.toLowerCase();
+    if (key === "a") {
+        // Play the audio
+        a.play();
+        
+        var button = document.querySelector("#a1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+    }
+});
+
+   // Get references to the button and s1udio element
+ var s1 = document.getElementById("s1");
+ var s = document.getElementById("s");
+
+ // Add a click event listener to the button
+ s1.addEventListener("click", function() {
+     s.play();
+
+     var button = document.querySelector("#s1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+ });
+ document.addEventListener("keydown", function(event) {
+    var key = event.key.toLowerCase();
+    if (key === "s") {
+        // Play the audio
+        s.play();
+        var button = document.querySelector("#s1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+    }
+});
+
+
+  // Get references to the button and s1udio element
+  var d1 = document.getElementById("d1");
+  var d = document.getElementById("d");
+ 
+  // Add a click event listener to the button
+  d1.addEventListener("click", function() {
+      d.play();
+
+      var button = document.querySelector("#d1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+  });
+  document.addEventListener("keydown", function(event) {
+    var key = event.key.toLowerCase();
+    if (key === "d") {
+        // Play the audio
+        d.play();
+
+        var button = document.querySelector("#d1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+    }
+});
+
+
+
+   // Get references to the button and s1udio element
+ var j1 = document.getElementById("j1");
+ var j = document.getElementById("j");
+
+ // Add a click event listener to the button
+ j1.addEventListener("click", function() {
+     j.play();
+
+     var button = document.querySelector("#j1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+ });
+ document.addEventListener("keydown", function(event) {
+    var key = event.key.toLowerCase();
+    if (key === "j") {
+        // Play the audio
+        j.play();
+
+        var button = document.querySelector("#j1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+    }
+});
+
+
+
+  // Get references to the button and s1udio element
+  var k1 = document.getElementById("k1");
+  var k = document.getElementById("k");
+ 
+  // Add a click event listener to the button
+  k1.addEventListener("click", function() {
+      k.play();
+
+      var button = document.querySelector("#k1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+  });
+  document.addEventListener("keydown", function(event) {
+    var key = event.key.toLowerCase();
+    if (key === "k") {
+        // Play the audio
+        k.play();
+
+        var button = document.querySelector("#k1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+    }
+});
+
+   // Get references to the button and s1udio element
+ var l1 = document.getElementById("l1");
+ var l = document.getElementById("l");
+
+ // Add a click event listener to the button
+ l1.addEventListener("click", function() {
+     l.play();
+
+     var button = document.querySelector("#l1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+ });
+ document.addEventListener("keydown", function(event) {
+    var key = event.key.toLowerCase();
+    if (key === "l") {
+        // Play the audio
+        l.play();
+
+        var button = document.querySelector("#l1");
+     button.classList.add("pressed");
+     setTimeout(function() {
+        button.classList.remove("pressed");
+      }, 500);
+    }
+});
